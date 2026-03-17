0b25edbe400a870900671df516ffb825b2d755aa
Valentinfrm <valentinfromm@proton.me>
2026-03-17 11:24:49 +0100

    update fixture to use requested params

--- conftest.py
+++ conftest.py
@@ -1,33 +1,10 @@
 # pytest configuration: adds root directory to Python path for module discovery
 import params
 import pytest
-import os
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
 def set_kem_params(request):
-    filename = os.path.basename(request.fspath)
-    
-    params.n = 256
-    params.q = 3329
-    
-    if "768" in filename:
-        params.k = 3
-        params.eta1 = 2
-        params.du = 10
-        params.dv = 4
-    elif "512" in filename:
-        params.k = 2
-        params.eta1 = 3
-        params.du = 10
-        params.dv = 4
-    elif "1024" in filename:
-        params.k = 4
-        params.eta1 = 2
-        params.du = 11
-        params.dv = 5
-    else:
-        # default: ML-KEM-768
-        params.k = 3
-        params.eta1 = 2
-        params.du = 10
-        params.dv = 4
+    kem_data = request.param # gets data from parameter
+    for key, value in kem_data["params"].items(): # sets params.key = value
+        setattr(params, key, value)
+    return kem_data
