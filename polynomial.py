1f578bbe14753bd311d640baba152e51ccce2cd0
Valentinfrm <valentinfromm@proton.me>
2026-03-18 20:14:48 +0100

    move field logic into poly

--- polynomial.py
+++ polynomial.py
@@ -1,6 +1,5 @@
+from ntt import mul_NTT
 import params
-from field import add, sub, mul, reduce
-from ntt import NTT, iNTT, mul_NTT
 
 class poly:
     """Polynomial in Rq = Zq[x]/(x^n + 1)"""
@@ -11,37 +10,21 @@ class poly:
     def __add__(self, poly_b):
         result = []
         for i in range(self.n):
-            tmp = add(self.coeff[i], poly_b.coeff[i])
+            tmp = (self.coeff[i] + poly_b.coeff[i]) % params.q
             result.append(tmp)
         return poly(result)
 
     def __sub__(self, poly_b):
         result = []
         for i in range(self.n):
-            tmp = sub(self.coeff[i], poly_b.coeff[i])
+            tmp = (self.coeff[i] - poly_b.coeff[i]) % params.q
             result.append(tmp)
         return poly(result)
 
     def __mul__(self, poly_b):
         """takes only NTT as input!"""
         return poly(mul_NTT(self.coeff, poly_b.coeff))
-        
-        """
-        else :
-            result = []
-            tmp = list(self.coeff) # copy coeffs to avoid overwriting while reading
-            for row in range(self.n):
-                c = 0
-                for col in range(self.n): 
-                    og_index = (row - col) % self.n # index in original poly (wraps around negacyclically)
-                    v = mul(tmp[og_index], poly_b.coeff[col])
-                    if col > row:
-                        v = -v # negate when index wrapped around (x^n = -1)
-                    c += v
-                result.append(reduce(c))
-            return poly(result)
-        """
-            
+         
     def poly_reduce(self):
         for i in range(len(self.coeff)):
-            self.coeff[i] = reduce(self.coeff[i])
+            self.coeff[i] = self.coeff[i] % params.q
